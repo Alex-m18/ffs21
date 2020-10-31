fd4ed7c01c49c55cbeefdc8534b6bd14c1aae9de
Alex-m18 <melnikov@tikhvin.org>
2020-11-01 01:46:56 +0300

    Server is listening on port message added

--- server.js
+++ server.js
@@ -229,3 +229,5 @@ app.use(router.allowedMethods());
 const port = process.env.PORT || 7070;
 const server = http.createServer(app.callback());
 server.listen(port);
+
+console.log(`Server is listening on port ${port}`);
