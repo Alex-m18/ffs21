f21dc5f0b71f53e2f73deb9be28f33adb1d8a410
Alex-m18 <melnikov@tikhvin.org>
2020-11-29 13:51:40 +0300

    Updates

---
+++ precache-manifest.a1e85197087994a5e1009ce0b022cd2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a24a53bed57be3c754177ec8d09396b",
+    "url": "/ffs21/index.html"
+  },
+  {
+    "revision": "714a938bf433d9df9fe7",
+    "url": "/ffs21/static/css/main.632c43a9.chunk.css"
+  },
+  {
+    "revision": "286ee1b4f4e9875087d3",
+    "url": "/ffs21/static/js/2.abbd72ff.chunk.js"
+  },
+  {
+    "revision": "d5003e14b193f7666444b7265ed368d9",
+    "url": "/ffs21/static/js/2.abbd72ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "714a938bf433d9df9fe7",
+    "url": "/ffs21/static/js/main.28fa8242.chunk.js"
+  },
+  {
+    "revision": "00c8d07f8e1d1991d827",
+    "url": "/ffs21/static/js/runtime-main.49e60a9c.js"
+  },
+  {
+    "revision": "00a1e8b6aaeb5dad292a7f3f8fe01a29",
+    "url": "/ffs21/static/media/background.00a1e8b6.jpg"
+  },
+  {
+    "revision": "54f4d94110abe9fc25c8e5e98826b4e3",
+    "url": "/ffs21/static/media/background.54f4d941.jpg"
+  }
+]);
